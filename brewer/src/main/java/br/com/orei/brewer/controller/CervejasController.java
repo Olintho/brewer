935740a53dabeb1b4d6fe14078b49542dba2f62f
Olintho Jesus <olinthojesus@yahoo.com.br>
2019-07-03 17:13:48 -0300

    Novo commit após merge from Office

--- brewer/src/main/java/br/com/orei/brewer/controller/CervejasController.java
+++ brewer/src/main/java/br/com/orei/brewer/controller/CervejasController.java
@@ -75,6 +75,7 @@ public class CervejasController {
 //		mv.addObject("cervejas", cervejas.filtrar(cervejaFilter, pageable));
 		
 		System.out.println("Primeiro commit no branch Office");
+		System.out.println("Merge ok. Upando from Home");
 
 		return mv;
 	}
