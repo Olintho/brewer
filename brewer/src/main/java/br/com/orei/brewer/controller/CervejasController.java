4233302575c556f6267b0a1dd8ca290aec54f759
Olintho Jesus <olinthojesus@yahoo.com.br>
2019-07-18 23:15:17 -0300

    Implementando ate cap 22.6

--- brewer/src/main/java/br/com/orei/brewer/controller/CervejasController.java
+++ brewer/src/main/java/br/com/orei/brewer/controller/CervejasController.java
@@ -1,5 +1,7 @@
 package br.com.orei.brewer.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
@@ -12,10 +14,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.orei.brewer.controller.page.PageWrapper;
+import br.com.orei.brewer.dto.CervejaDTO;
 import br.com.orei.brewer.model.Cerveja;
 import br.com.orei.brewer.model.Origem;
 import br.com.orei.brewer.model.Sabor;
@@ -72,11 +76,13 @@ public class CervejasController {
 		PageWrapper<Cerveja> paginaWrapper = new PageWrapper<>(cervejas.filtrar(cervejaFilter, pageable), httpServletRequest);
 		
 		mv.addObject("pagina", paginaWrapper);
-//		mv.addObject("cervejas", cervejas.filtrar(cervejaFilter, pageable));
-		
 		return mv;
 	}
-	
+
+	@GetMapping("/filtro")
+	public @ResponseBody List<CervejaDTO> pesquisar(String skuOuNome){
+		return cervejas.porSkuOuNome(skuOuNome);
+	}
 
 	
 }
